f899e2d56c18584a751a7ba4df88adf65a4671bc
DavidIRL <david.white0911@gmail.com>
2024-07-25 22:47:06 -0400

    update ElapsedTime in format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <format>
 
 #include "format.h"
 
@@ -8,4 +9,20 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    string hr = static_cast<string>(static_cast<int>(seconds / 3600));
+    string min = static_cast<string>(static_cast<int>((seconds % 3600)/ 60));
+    string sec = static_cast<string>(static_cast<int>((seconds % 3600)% 60));
+    // prepend 0s as needed for HH:MM:SS
+    string hrs = string(2-hr.length(), "0") + hr;
+    string mins = string(2-min.length(), "0") + min;  
+    string secs = string(2-sec.length(), "0") + sec;
+    string result = "";
+  
+    if (hrs.compare("00") != 0) {
+        result = std::format("{}:{}:{}", hrs, mins, secs);
+    } else {
+        result = std::format("{}:{}", mins, secs);
+    }
+    return result;
+}
